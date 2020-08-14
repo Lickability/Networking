17e507c185711c48cdbbe79f53f8a743af815a0b
Michael Liberatore <mi@chael.co>
2020-08-14 14:47:56 -0400

    Removes explicit path

--- Package.swift
+++ Package.swift
@@ -8,5 +8,5 @@ let package = Package(
     name: name,
     platforms: [.iOS(.v13)],
     products: [.library(name: name, targets: [name])],
-    targets: [.target(name: name, path: "Sources/Networking")]
+    targets: [.target(name: name)]
 )
