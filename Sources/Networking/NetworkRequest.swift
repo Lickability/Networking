3a67083729cca01900bda98e2eefb0d18dfb25a1
Michael Amundsen <michaelamundsen@gmail.com>
2020-08-19 14:17:50 -0400

    adding headers

--- Sources/Networking/NetworkRequest.swift
+++ Sources/Networking/NetworkRequest.swift
@@ -26,6 +26,9 @@ public protocol NetworkRequest {
     /// A list of query parameters to add to the URL.
     var queryParameters: [URLQueryItem] { get }
     
+    /// The HTTP header fields and their values to set on the request.
+    var httpHeaders: [String: String] { get }
+    
     /// The data to send in the HTTPBody.
     var httpBody: Data? { get }
 }
@@ -61,6 +64,10 @@ public extension NetworkRequest {
 
     var queryParameters: [URLQueryItem] {
         return []
+    }
+    
+    var httpHeaders: [String: String] {
+        return [:]
     }
     
     var httpBody: Data? {
