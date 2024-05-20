1c21a67b1ec475de85c28872f7d6f0199a7e5112
Ashli Rankin <ashlirankin18@gmail.com>
2024-05-20 15:34:32 -0400

    Allows user to explicitly set the scheduler

--- Sources/Networking/NetworkRequestStateController.swift
+++ Sources/Networking/NetworkRequestStateController.swift
@@ -94,13 +94,15 @@ public final class NetworkRequestStateController {
     /// - Parameters:
     ///   - request: The request to send.
     ///   - requestBehaviors: Additional behaviors to append to the request.
-    public func send(request: any NetworkRequest, requestBehaviors: [RequestBehavior] = [], retryCount: Int = 2) {
+    ///   - retryCount: The number of times the action can be retried.
+    ///   - scheduler: The scheduler to receive the call on. The default value is `DispatchQueue.main`.
+    public func send(request: any NetworkRequest, requestBehaviors: [RequestBehavior] = [], retryCount: Int = 2, scheduler: some Scheduler = DispatchQueue.main) {
         requestStatePublisher.send(.inProgress)
         
         requestPerformer.send(request, requestBehaviors: requestBehaviors)
             .retry(retryCount)
             .mapAsResult()
-            .receive(on: DispatchQueue.main)
+            .receive(on: scheduler)
             .sink(receiveValue: { [requestStatePublisher] result in
                 requestStatePublisher.send(.completed(result))
             })
