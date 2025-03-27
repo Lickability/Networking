1b9dfca5590875618940d3e10912f89b4c644e77
Ken Ackerson <ken@lickability.net>
2025-03-27 11:37:06 -0400

    remove main actor

--- Sources/Networking/NetworkRequestPerformer.swift
+++ Sources/Networking/NetworkRequestPerformer.swift
@@ -27,7 +27,6 @@ public protocol NetworkRequestPerformer: Sendable {
     ///   - scheduler: The scheduler to receive the call on. The scheduler passed in must match the `@MainActor` requirement to avoid data races.
     ///   - requestBehaviors: The behaviors to apply to the given request.
     /// - Returns: Returns a publisher that can be subscribed to, that performs the given request with the given behaviors.
-    @MainActor
     @discardableResult func send(_ request: any NetworkRequest, scheduler: some Scheduler, requestBehaviors: [RequestBehavior]) -> AnyPublisher<NetworkResponse, NetworkError>
     
     /// Performs the given request with the given behaviors returning a `NetworkResponse` with async/await, or throwing an error if unsuccessful.
